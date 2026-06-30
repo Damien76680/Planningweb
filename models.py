bf6df6e95fdc371ef5b340f79d8708b546e272a0
Damien76680 <damien_76140@hotmail.fr>
2026-06-30 08:39:22 +0200

    Add Task model with various attributes

---
+++ models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(200))
+    duree = db.Column(db.Float)
+    etat = db.Column(db.String(50))
+    temps_fait = db.Column(db.Float, default=0)
+    start_time = db.Column(db.String, nullable=True)
+    ordre = db.Column(db.Integer)
+    deadline = db.Column(db.String, nullable=True)
