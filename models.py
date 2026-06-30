2e74a077e8b4f1925c00d4b2d340083198a02e53
Damien76680 <damien_76140@hotmail.fr>
2026-06-30 23:51:37 +0200

    Add client field and new models for Holiday and Settings
    Added client field to Task model and created Holiday and Settings models.

--- models.py
+++ models.py
@@ -5,7 +5,16 @@ db = SQLAlchemy()
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nom = db.Column(db.String(200))
+    client = db.Column(db.String(200))
     duree = db.Column(db.Float)
     etat = db.Column(db.String(50))
     ordre = db.Column(db.Integer)
     deadline = db.Column(db.String, nullable=True)
+
+class Holiday(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String, unique=True)
+
+class Settings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.Text)
